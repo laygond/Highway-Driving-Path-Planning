1e9d74f9dcd4eed70543e9b61c66599d701eaf54
laygond <laygond_bryan@hotmail.com>
2020-06-26 19:24:39 -0400

    :sparkles: additional dependencies

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-sudo apt-get install libuv1-dev libssl-dev libz-dev
+sudo apt-get update
+sudo apt-get install libuv1-dev libssl-dev libz-dev gcc g++ cmake make
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
 git checkout e94b6e1
